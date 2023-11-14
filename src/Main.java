7af997a3e81947db5c687e160c882a8167b9083a
kuzubaevMax <kuzuvaevazamattalasovich@gmail.com>
2023-11-14 16:20:59 +0300

    init 1

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         for (int i = 1; i <= 5; i++) {
             System.out.print("i = " + i);
             System.out.println(" ");
+            System.out.println("Azamat");
         }
     }
 }
